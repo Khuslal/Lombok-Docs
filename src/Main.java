4aaca37c8ec65ecc13dfd51007b98d0b3e167572
Khuslal Gupta <guptakhuslal@gmail.com>
2026-06-30 22:13:24 +0545

    Lombok Key Attribute, Grouped and Constructor Annotations

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ class Main {
         *   @ToString => Generates a toString() method incorporating your class attributes.
         *   @EqualsAndHashCode => Generates equals() & hashcode() methods using your fields.
         *
+        *
         * 2. Grouped Annotations
         *   Instead of writing them seperately, you can bundle them together using shortcuts :-
         *
@@ -18,6 +19,30 @@ class Main {
         *           A convenient bundle that acts as @ToString, @EqualsAndHashCode,
         *           @Getter on all fields, @Setter on all non-final fields, and
         *           @RequiredArgsConstructor.
+        *       @Valule :
+        *           The immutable equivalent to @Data. It makes all fields private & final, & only generates getters (no setters).
+        *
+        * 3. Constructor Annotations
+        *
+        *       @NoArgsConstructor :
+        *           Creates an empty constructor.
+        *
+        *       @RequiredArgsConstructor :
+        *           Generates a constructor for all fields marked 'final' or '@NonNull'.
+        *
+        *       @AllArgsConstructor :
+        *           Generates a constructor with parameters for all attributes.
+        *
+        *
+        * ===========Best Practices and Pitfalls=============
+        *
+        * JPA Entities :
+        *   Use @Data with caution on JPA entities with bidirectional relationships.
+        *   It can cause infinite loops in toString & hashCode methods.
+        *
+        * Immutability :
+        *   When dealing with unchangable objects, prefer to @Value instead of @Data.
+        *
         * */
     }
 }
